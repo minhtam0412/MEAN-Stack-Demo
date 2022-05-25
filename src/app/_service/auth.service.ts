813f0343c4652b9fe0abdac835650e08a18f2fa6
Tam Ngo <minhtam0412@gmail.com>
2022-05-25 14:21:45 +0700

    update code

---
+++ src/app/_service/auth.service.ts
@@ -0,0 +1,31 @@
+//https://www.bezkoder.com/angular-12-jwt-auth/#Add_Bootstrap_to_Angular_project
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+const AUTH_API = 'http://localhost:4000/';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'login', {
+      email,
+      password
+    }, httpOptions);
+  }
+
+  register(userName: string, email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'register', {
+      userName,
+      email,
+      password
+    }, httpOptions);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,28 @@ import {EmployeeEditComponent} from './components/employee-edit/employee-edit.co
 import {EmployeeListComponent} from './components/employee-list/employee-list.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeCreateComponent,
     EmployeeEditComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +38,7 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule {
