89fd980f1e573c9df18ae0d0c46ae9cd73806c6e
Ngô Minh Tâm <minhtam0412@gmail.com>
2022-05-27 00:53:00 +0700

    update content api

--- backend/controllers/board.controller.js
+++ backend/controllers/board.controller.js
@@ -1,15 +1,15 @@
 exports.allAccess = (req, res) => {
-  res.status(200).send(`Public content`);
+  res.status(200).send(`Public content. Every one can see, not require login`);
 };
 
 exports.userBoard = (req, res) => {
-  res.status(200).send('User content');
+  res.status(200).send('User content. Only logined user can see');
 };
 
 exports.adminBoard = (req, res) => {
-  res.status(200).send('Admin content');
+  res.status(200).send('Admin content. Only Admin Role can see');
 };
 
 exports.moderatorBoard = (req, res) => {
-  res.status(200).send("Moderator Content.");
+  res.status(200).send("Moderator Content. Only moderator can see");
 };
